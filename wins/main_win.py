0c3f9acae43e57b29add05f140c2e3ce330ea7c8
SaySmokeGraf <seme4ka.elizovo@gmail.com>
2025-11-27 14:11:37 +0300

    Добавление менеджера графиков

--- wins/main_win.py
+++ wins/main_win.py
@@ -2,10 +2,11 @@
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QCheckBox, QMainWindow
-import pyqtgraph as pg
+import pyqtgraph as pgraph
 
 from config import PATH_ICON
 from ui.mainGUI import Ui_MainWindow
+from utils.graph_manager import GraphManager
 from utils.methods import (
     calculate_method_1, calculate_method_2, calculate_method_3
 )
@@ -25,7 +26,8 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QIcon(PATH_ICON))
 
         # описание создаваемых далее атрибутов класса
-        self._checkboxes_leg: list[QCheckBox]  # чекбоксы вкл/выкл легенд
+        self._checkboxes_leg: tuple[QCheckBox]  # чекбоксы вкл/выкл легенд
+        self._graph_managers: list[GraphManager]  # менеджеры графиков
 
         # дополнительный ГПИ
         self._setup_additional_ui()
@@ -62,62 +64,26 @@ class MainWindow(QMainWindow):
     def _setup_graphs(self) -> None:
         """Разворачивает все графики для методов."""
         # цвета графиков
-        pg.setConfigOption('background', 'w')
-        pg.setConfigOption('foreground', 'k')
+        pgraph.setConfigOption('background', 'w')
+        pgraph.setConfigOption('foreground', 'k')
 
-        # формируем необходимые списки графиков и их элементов
-        self._graphs = []
-        self._plot_data = []
-        self._plot_outline = []
-        self._plot_stations = []
-        self._plot_legends = []
+        # формируем список менеджеров графиков
+        self._graph_managers = []
 
         # пробежка по 3 методам для разворачивания для них графиков
         for i in range(3):
             self._temp_h_layouts[i].removeWidget(self._temp_lbls_to_morph[i])
             self._temp_lbls_to_morph[i].deleteLater()
             self._temp_lbls_to_morph[i] = None
-
-            self._graphs.append(pg.PlotWidget(self._temp_frames_graph[i]))
-            self._temp_h_layouts[i].addWidget(self._graphs[i])
-            self._graphs[i].setLabel('left', 'Ось Y')
-            self._graphs[i].setLabel('bottom', 'Ось X')
-            self._graphs[i].showGrid(x=True, y=True)
-
-            self._plot_data.append(self._graphs[i].plot([], [], pen=None, symbol='o', symbolSize=5,
-                                  symbolPen='b', symbolBrush='b', name='Подходящая область'))
-            self._plot_outline.append(self._graphs[i].plot([], [], pen=None, symbol='o', symbolSize=5,
-                                  symbolPen='k', symbolBrush='k', name='Контур подходящей области'))
-            self._plot_stations.append(self._graphs[i].plot([], [], pen=None, symbol='t1', symbolSize=20,
-                                      symbolPen='r', symbolBrush='r', name='Маяки'))
             
-            self._plot_legends.append(None)
-            self._set_legend_on_graph(i, True)
+            new_graph = pgraph.PlotWidget(self._temp_frames_graph[i])
+            self._temp_h_layouts[i].addWidget(new_graph)
+            self._graph_managers.append(GraphManager(new_graph))
         
         # удаляем временные списки и кортежи
         del self._temp_frames_graph
         del self._temp_h_layouts
         del self._temp_lbls_to_morph
-
-    def _set_legend_on_graph(self, n: int, enabled: bool) -> None:
-        """Установить легенду на графике.
-        
-        :param n: номер графика от 0 до 2
-        :type n: int
-        :param enabled: флаг включенности легенды
-        :type enabled: bool
-        """
-        if enabled:
-            if self._plot_legends[n] == None:
-                self._plot_legends[n] = pg.LegendItem((80,60), offset=(70,20), frame=True, brush='w')
-                self._plot_legends[n].setParentItem(self._graphs[n].graphicsItem())
-                self._plot_legends[n].addItem(self._plot_data[n], 'Подходящая область')
-                self._plot_legends[n].addItem(self._plot_outline[n], 'Контур подходящей области')
-                self._plot_legends[n].addItem(self._plot_stations[n], 'Маяки')
-                self._plot_legends[n].setZValue(1)
-        else:
-            self._graphs[n].scene().removeItem(self._plot_legends[n])
-            self._plot_legends[n] = None
     
     def _active_elems_enabled(self, enabled: bool) -> None:
         """Включение/выключение активных (интерактивных) элементов ГПИ.
@@ -133,31 +99,8 @@ class MainWindow(QMainWindow):
         :param n: номер графика от 0 до 2
         :type n: int
         """
-        self._set_legend_on_graph(n, self._checkboxes_leg[n].isChecked())
-    
-    def _upd_graph(self, n: int, X: list[float], Y: list[float],
-                   Xout: list[float], Yout: list[float],
-                   Xm: list[float], Ym: list[float]) -> None:
-        """Обновляет данные на графике.
-
-        :param n: номер графика от 0 до 2
-        :type n: int
-        :param X: список координат X подходящих точек
-        :type X: list[float]
-        :param Y: список координат Y подходящих точек
-        :type Y: list[float]
-        :param Xout: список координат X контура подходящих точек
-        :type Xout: list[float]
-        :param Yout: список координат Y контура подходящих точек
-        :type Yout: list[float]
-        :param Xm: список координат X маяков
-        :type Xm: list[float]
-        :param Ym: список координат Y маяков
-        :type Ym: list[float]
-        """
-        self._plot_data[n].setData(X, Y)
-        self._plot_outline[n].setData(Xout, Yout)
-        self._plot_stations[n].setData(Xm, Ym)
+        enabled = self._checkboxes_leg[n].isChecked()
+        self._graph_managers[n].enable_legend(enabled)
     
     def _calculate_method_1(self) -> None:
         """Расчет рабочей зоны по первому методу (разностно-дальномерный).
@@ -185,7 +128,7 @@ class MainWindow(QMainWindow):
                                         sigma_r_allow, sigma_t, P, r)
         
         # обновление графика и включение активных элементов
-        self._upd_graph(0, *graph_data)
+        self._graph_managers[0].update(*graph_data)
         self._active_elems_enabled(True)
 
     def _calculate_method_2(self) -> None:
@@ -214,7 +157,7 @@ class MainWindow(QMainWindow):
                                         sigma_d, sigma_r, P, r)
 
         # обновление графика и включение активных элементов
-        self._upd_graph(1, *graph_data)
+        self._graph_managers[1].update(*graph_data)
         self._active_elems_enabled(True)
 
     def _calculate_method_3(self) -> None:
@@ -243,7 +186,7 @@ class MainWindow(QMainWindow):
                                         sigma_d, sigma_theta, P, r)
 
         # обновление графика и включение активных элементов
-        self._upd_graph(2, *graph_data)
+        self._graph_managers[2].update(*graph_data)
         self._active_elems_enabled(True)
 
 
